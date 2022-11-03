6f95e7b435ba1da23ce32be8d9a43073b51a2c64
Roger <roger.vieira@icomp.ufam.edu.br>
2022-11-03 17:58:34 -0400

    Adicionando arquivos importantes

---
+++ aosp/device/generic/goldfish/x86_64-kernel.mk
@@ -0,0 +1,14 @@
+TARGET_KERNEL_USE ?= 5.10
+
+# KERNEL_MODULES_PATH := kernel/prebuilts/common-modules/virtual-device/$(TARGET_KERNEL_USE)/x86-64   // Comente essa linha
+KERNEL_MODULES_PATH := kernel_recompilado
+
+KERNEL_MODULES_EXCLUDE := \
+    $(KERNEL_MODULES_PATH)/virt_wifi.ko \
+    $(KERNEL_MODULES_PATH)/virt_wifi_sim.ko
+
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES += \
+    $(filter-out $(KERNEL_MODULES_EXCLUDE), $(wildcard $(KERNEL_MODULES_PATH)/*.ko))
+
+# EMULATOR_KERNEL_FILE := kernel/prebuilts/$(TARGET_KERNEL_USE)/x86_64/kernel-$(TARGET_KERNEL_USE)    // Comente essa linha
+EMULATOR_KERNEL_FILE := kernel_recompilado/bzImage
